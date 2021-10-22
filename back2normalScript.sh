b1de06e5ac53cd4b09263b5758a480215c71c56f
bladykast <bladykas.thomas@gmail.com>
2021-10-22 02:22:12 -0400

    Add files via upload

--- back2normalScript.sh
+++ back2normalScript.sh
@@ -18,9 +18,9 @@ git -c advice.detachedHead=false checkout $latestTag
 
 installBuild () {
 # For fprintd and libprintf, both meson and ninja are used to build from source and install
-meson builddir
-ninja -C builddir install
-{
+sudo -A meson builddir
+sudo -A ninja -C builddir install
+}
 
 boldEcho () {
 # Bolds print output for visibility
@@ -29,12 +29,11 @@ echo -e "\\033[1m> $1 <\033[0m"
 
 updateUpgrade () {
 # Good ol' commands, here for no particular reason
-apt update
-apt upgrade -y
+sudo -A apt update
+sudo -A apt upgrade -y
 }
 
 ##### End of Functions #####
-
 
 # Must not prefix with sudo when calling script
 if [[ $(id -u) == 0 ]]; then
@@ -46,41 +45,39 @@ fi
 tempdir=$(mktemp -d)
 cd $tempdir
 
-# Get current user, for deescalation
-currentuser=$(whoami)
+# Call for Escalation; cache password
+touch elevate.sh
+chmod +x elevate.sh
+echo '#!/bin/bash' | tee elevate.sh
+echo 'kdialog --password "Password required to proceed"' | tee -a elevate.sh
 
+export SUDO_ASKPASS=$tempdir/elevate.sh
 
 ##### Beginning of dependency fetch ######
-
-# Call for Escalation
-
-#
 
 boldEcho 'Checking for (and installing) updates...'
 updateUpgrade
 
 # boldEcho 'Installing acpi_call for custom fan control...'
-# apt install -y acpi-call
-# touch /etc/modules-load.d/acpi-call.conf
-# echo "acpi_call" | tee -a /etc/modules-load.d/acpi-call.conf
+# sudo -A apt install -y acpi-call
+# sudo -A touch /etc/modules-load.d/acpi-call.conf
+# echo "acpi_call" | sudo -A tee -a /etc/modules-load.d/acpi-call.conf
 
 boldEcho 'Installing asusctl dependencies...'
-apt install -y libclang-dev libudev-dev cargo
+sudo -A apt install -y libclang-dev libudev-dev cargo
 
 boldEcho 'Installing supergfxctl dependencies...'
-apt install -y curl git build-essential
+sudo -A apt install -y curl git build-essential
 
 boldEcho 'Installing libprintf and fprintd dependencies...'
-apt purge --auto-remove fprintd libfprint-2-2
-apt install -y python3-venv python3-pip git gettext valgrind libpam-fprintd
-pip3 install --no-input meson libglib2.0-dev libgusb-dev libgrepository1.0-dev gtk-doc-tools libpolkit-gobject-1-dev libsystemd-dev libpam0g-dev libpam-wrapper libfprint-2-dev python3-pypamtest libxml2-utils libdbus-1-dev ninja gobject python-dbusmock
-
-## De-escalate things ##
+sudo -A apt purge -y --auto-remove fprintd libfprint-2-2
+sudo -A apt install -y python3-venv python3-pip git gettext valgrind libpam-fprintd
+sudo -A pip3 install --no-input meson libglib2.0-dev libgusb-dev libgrepository1.0-dev gtk-doc-tools libpolkit-gobject-1-dev libsystemd-dev libpam0g-dev libpam-wrapper libfprint-2-dev python3-pypamtest libxml2-utils libdbus-1-dev ninja gobject python-dbusmock
 
 ##### Un-escalated things  #####
 cd $tempdir
 
-boldEcho 'Fetching Rustup...' # Required for supergfxctl
+boldEcho 'Fetching Rustup...' # Required for supergfxctl? fix this
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs
 source ~/.bash_profile
 source ~/.profile
@@ -89,6 +86,7 @@ cd $tempdir
 
 boldEcho 'Fetching asusctl...'
 getLatest https://gitlab.com/asus-linux/asusctl.git
+asusctl_version=$latestTag
 
 boldEcho 'Building asusctl...'
 make
@@ -96,6 +94,7 @@ cd $tempdir
 
 boldEcho 'Fetching supergfxctl...'
 getLatest https://gitlab.com/asus-linux/supergfxctl.git
+supergfxctl_version=$latestTag
 
 boldEcho 'Building supergfxctl...'
 make
@@ -103,6 +102,7 @@ cd $tempdir
 
 boldEcho 'Fetching libprinf...'
 getLatest https://gitlab.freedesktop.org/libfprint/libfprint.git
+libprintf_version=$latestTag
 
 boldEcho 'Building & installing libprintf...'
 installBuild
@@ -110,41 +110,61 @@ cd $tempdir
 
 boldEcho 'Fetching fprintd...'
 getLatest https://gitlab.freedesktop.org/libfprint/fprintd.git
+fprintd_version=$latestTag
 
 boldEcho 'Building & installing fprintd...'
 installBuild
 cd $tempdir
 
-
 ##### Escalated, again?? #####
 
 boldEcho 'Installing asusctl...'
 cd $tempdir/asusctl/
-make install
+sudo -A make install
 
 boldEcho 'Starting asusd service...'
-systemctl enable asusd
-systemctl start asusd
+sudo -A systemctl enable asusd
+sudo -A systemctl start asusd
 
 boldEcho 'Installing supergfxctl...'
 cd $tempdir/supergfxctl/
-make install
+sudo -A make install
 
 boldEcho 'Starting supergfxd service...'
-systemctl enable supergfxd.service
-systemctl start supergfxd.service
+sudo -A systemctl enable supergfxd.service
+sudo -A systemctl start supergfxd.service
+
+boldEcho 'Would you like to enable notifications for KDE/Gnome? [Y/n]'
+read remove
+if [[ $remove == 'n' ]]; then
+
+    boldEcho 'To enable notifications in the future, run these: '
+    echo '$ systemctl --user enable asus-notify.service'
+    echo '$ systemctl --user start asus-notify.service'
+    else
+    systemctl --user enable asus-notify.service
+    systemctl --user start asus-notify.service
+    echo 'Notifications enabled.'
+fi
+
 
 ##### Cleanup #####
-boldEcho 'Done! Would you like to remove the build directories?'
-rm -rf $tempdir # Risky!
+boldEcho 'Done!'
 
-##### De-escalate, again... #####
+boldEcho 'Program versions installed:'
+echo asusctl: $asusctl_version
+echo supergfxctl: $supergfxctl_version
+echo libprinf: $libprintf_version
+echo fprintd: $fprintd_version
+echo ' '
 
-boldEcho 'Would you like to enable asusctl desktop notifications? (KDE/Gnome)'
+boldEcho 'Would you like to remove the build directories? [y/N]'
+read remove
+if [[ $remove == 'y' ]]; then
+    rm -rf $tempdir
+    echo 'Build directory removed.'
+    else
+    echo Build files remain at $tempdir
+fi
 
-# This *can't* be run as root
-boldEcho 'Enabling notifications for KDE/GNOME...'
-systemctl --user enable asus-notify.service
-systemctl --user start asus-notify.service
-
-
+exit 0
